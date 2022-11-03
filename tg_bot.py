9ce5d2f1481b3cbcfd160c5202e3f63118d75c9a
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2022-11-03 03:43:27 +0300

    Initial commit

---
+++ tg_bot.py
@@ -0,0 +1,87 @@
+import logging
+
+import redis
+import telegram
+from environs import Env
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
+
+logger = logging.getLogger(__name__)
+
+START, SOLUTION = range(2)
+
+
+def start(bot, update):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счет']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(
+        text='Привет, я бот для викторин',
+        reply_markup=reply_markup,
+    )
+    return START
+
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def handle_new_question_request(bot, update):
+    random_question = redis_questions.randomkey()
+    update.message.reply_text(random_question)
+    redis_user_history.set(update.message['chat']['id'], random_question)
+    return SOLUTION
+
+
+def handle_solution_attempt(bot, update):
+    last_question = redis_user_history.get(update.message['chat']['id'])
+    answer = redis_questions.get(last_question)
+    if update.message.text.lower() == answer.split('.')[0].strip().lower():
+        update.message.reply_text('Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»')
+        return START
+    else:
+        update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+        return SOLUTION
+
+
+def handle_give_up_request(bot, update):
+    last_question = redis_user_history.get(update.message['chat']['id'])
+    answer = redis_questions.get(last_question)
+    update.message.reply_text('Правильный ответ: {}'.format(answer))
+    return handle_new_question_request(bot, update)
+
+
+def main():
+    updater = Updater(telegram_token)
+    dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            START: [RegexHandler('^Новый вопрос$', handle_new_question_request)],
+            SOLUTION: [
+                RegexHandler('^Новый вопрос$', handle_new_question_request),
+                RegexHandler('^Сдаться$', handle_give_up_request),
+                MessageHandler(Filters.text, handle_solution_attempt),
+            ],
+        },
+        fallbacks=[]
+    )
+
+    dp.add_handler(conv_handler)
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    redis_host = env.str('REDIS_HOST')
+    redis_port = env.str('REDIS_PORT')
+    telegram_token = env.str('TELEGRAM_TOKEN')
+    redis_questions = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+    redis_user_history = redis.Redis(host=redis_host, port=redis_port, db=1, decode_responses=True)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    main()
---
+++ vk_bot.py
@@ -0,0 +1,78 @@
+import redis
+import vk_api as vk
+from environs import Env
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+
+
+def handle_new_question_request(event, vk_api):
+    random_question = redis_questions.randomkey()
+    vk_api.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=random_question
+    )
+    redis_user_history.set(event.user_id, random_question)
+
+
+def handle_solution_attempt(event, vk_api):
+    last_question = redis_user_history.get(event.user_id)
+    if not last_question:
+        handle_new_question_request(event, vk_api)
+        return
+    answer = redis_questions.get(last_question)
+    if event.text.lower() == answer.split('.')[0].strip().lower():
+        vk_api.messages.send(
+            user_id=event.user_id,
+            random_id=get_random_id(),
+            keyboard=keyboard.get_keyboard(),
+            message='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
+        )
+    else:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            random_id=get_random_id(),
+            keyboard=keyboard.get_keyboard(),
+            message='Неправильно… Попробуешь ещё раз?'
+        )
+
+
+def handle_give_up_request(event, vk_api):
+    last_question = redis_user_history.get(event.user_id)
+    answer = redis_questions.get(last_question)
+    vk_api.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message='Правильный ответ: {}'.format(answer)
+    )
+    handle_new_question_request(event, vk_api)
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    redis_host = env.str('REDIS_HOST')
+    redis_port = env.str('REDIS_PORT')
+    redis_questions = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+    redis_user_history = redis.Redis(host=redis_host, port=redis_port, db=1, decode_responses=True)
+    vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
+    vk_api = vk_session.get_api()
+
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button('Мой счет', color=VkKeyboardColor.PRIMARY)
+
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            if event.text == 'Новый вопрос':
+                handle_new_question_request(event, vk_api)
+            elif event.text == 'Сдаться':
+                handle_give_up_request(event, vk_api)
+            else:
+                handle_solution_attempt(event, vk_api)
